0f88349a48bd3e6715216c0152b8289083f29c40
Jonahcas2 <casablancaazure@gmail.com>
2025-09-22 15:23:17 -0400

    Added Swagger functionality

--- Application/__init__.py
+++ Application/__init__.py
@@ -6,6 +6,16 @@ from .Blueprints.mechanics import mechanics_bp
 from .Blueprints.service_tickets import tickets_bp
 from .Blueprints.inventory import inventory_bp
 
+from flask_swagger_ui import get_swaggerui_blueprint
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.yaml'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={'app_name': "My API"}
+)
+
+
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(f'config.{config_name}')
@@ -21,5 +31,6 @@ def create_app(config_name):
     app.register_blueprint(mechanics_bp)
     app.register_blueprint(tickets_bp)
     app.register_blueprint(inventory_bp)
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
     return app
