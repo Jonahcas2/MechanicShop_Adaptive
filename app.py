137a2269ba1033dff090579db64abb2cf81b8796
Jonahcas2 <casablancaazure@gmail.com>
2025-10-06 14:32:06 -0400

    Deployment changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from Application import create_app
 from Application.models import db
 
-app = create_app('DevConfig')
+app = create_app('ProductionConfig')
 
 # Create table
 with app.app_context():
--- config.py
+++ config.py
@@ -9,6 +9,6 @@ class TestConfig:
     DEBUG = True
     CACHE_TYPE = 'SimpleCache'
 
-class ProdConfig:
+class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     CACHE_TYPE = "SimpleCache"
